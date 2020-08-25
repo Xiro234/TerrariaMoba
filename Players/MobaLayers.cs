03fafcc4ddbddbfd24fc3438a6fc2fd1fe42c3ec
Xiro234 <xiro234@gmail.com>
2020-08-25 12:56:12 -0400

    Fixed Sylvia Effects.

--- Players/MobaLayers.cs
+++ Players/MobaLayers.cs
@@ -22,6 +22,39 @@ namespace TerrariaMoba.Players {
                 Main.playerDrawData.Add(data);
             }
             
+            if(modPlayer.SylviaEffects.JunglesWrath) {
+                Texture2D texture = mod.GetTexture("Textures/Sylvia/JunglesWrath/JunglesWrath1");
+                bool draw = true;
+                switch (modPlayer.SylviaEffects.JunglesWrathCount) {
+                    case(1):
+                        texture = mod.GetTexture("Textures/Sylvia/JunglesWrath/JunglesWrath1");
+                        break;
+                    case(2):
+                        texture = mod.GetTexture("Textures/Sylvia/JunglesWrath/JunglesWrath2");
+                        break;
+                    case(3):
+                        texture = mod.GetTexture("Textures/Sylvia/JunglesWrath/JunglesWrath3");
+                        break;
+                    case(4):
+                        texture = mod.GetTexture("Textures/Sylvia/JunglesWrath/JunglesWrath4");
+                        break;
+                    default:
+                        draw = false;
+                        break;
+                }
+
+                if (draw) {
+                    int drawX = (int) (drawInfo.position.X + drawPlayer.width / 2f - Main.screenPosition.X);
+                    int drawY = (int) (drawInfo.position.Y + (drawPlayer.height - 56f) - Main.screenPosition.Y);
+                    DrawData data = new DrawData(texture, new Vector2(drawX, drawY),
+                        new Rectangle(0, 0, texture.Width, texture.Height),
+                        Lighting.GetColor((int) ((drawInfo.position.X + drawPlayer.width / 2f) / 16f),
+                            (int) ((drawInfo.position.Y + drawPlayer.height) / 16f)), 0f,
+                        new Vector2(texture.Width / 2f, texture.Height / 2f), 1f, SpriteEffects.None, 0);
+                    Main.playerDrawData.Add(data);
+                }
+            }
+            
             if (modPlayer.FlibnobEffects.TitaniumShell) {
                 Texture2D texture = mod.GetTexture("Textures/Flibnob/TitaniumShell");
 
