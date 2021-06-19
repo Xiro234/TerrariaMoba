0fca9d01bdb4496960385ec82f68fdc0af226405
DreadnovaBlade <worldofmythril@gmail.com>
2021-06-19 12:55:56 +0100

    Starting Jorm

--- Abilities/Jorm/Consecration.cs
+++ Abilities/Jorm/Consecration.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
 using TerrariaMoba.Enums;
 
 namespace TerrariaMoba.Abilities.Jorm {
@@ -7,8 +10,21 @@ namespace TerrariaMoba.Abilities.Jorm {
 
         public override Texture2D Icon { get => TerrariaMoba.Instance.GetTexture("Textures/Blank"); }
 
+        public const float CONSEC_SPREAD_RANGE = 500f;
+        public const int CONSEC_DURATION = 300;
+
         public override void OnCast() {
             //TODO - consecrate ground in ~ radius, +healeff of allies, -healeff of enemies on top.
+            if (Main.netMode != NetmodeID.Server && Main.myPlayer == User.whoAmI) {
+                Projectile proj = Projectile.NewProjectileDirect(User.Center, Vector2.Zero,
+                    TerrariaMoba.Instance.ProjectileType("Consecration"), 0, 0, User.whoAmI);
+
+                Projectiles.Jorm.Consecration consec = proj.modProjectile as Projectiles.Jorm.Consecration;
+                if (consec != null) {
+                    consec.ConsecSpread = CONSEC_SPREAD_RANGE;
+                    consec.ConsecDuration = CONSEC_DURATION;
+                }
+            }
         }
     }
 }
---
+++ Projectiles/Jorm/Consecration.cs
@@ -0,0 +1,58 @@
+﻿﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace TerrariaMoba.Projectiles.Jorm {
+    public class Consecration : ModProjectile {
+        public float ConsecSpread { get; set; }
+        public float ConsecDuration { get; set; }
+
+        public override void SetDefaults() {
+            projectile.width = 20;
+            projectile.height = 20;
+            projectile.friendly = true;
+            projectile.hostile = false;
+            projectile.timeLeft = 1000;
+            projectile.tileCollide = false;
+            projectile.alpha = 255;
+            projectile.penetrate = -1;
+
+            ConsecSpread = Abilities.Jorm.Consecration.CONSEC_SPREAD_RANGE;
+            ConsecDuration = Abilities.Jorm.Consecration.CONSEC_DURATION;
+        }
+
+        public override void AI() {
+            if (projectile.timeLeft == 1000) {
+                projectile.timeLeft = (int) ConsecDuration;
+            }
+
+            projectile.position.Y = GetYPos() - 2;
+            
+            if (projectile.width < ConsecSpread) {
+                projectile.width += 5;
+                projectile.position.X -= 2.5f;
+            }
+
+            for (int d = 0; d < 20; d++) {
+                Dust.NewDust(projectile.position, projectile.width, projectile.height, 269, 0, 0, 50);
+            }
+
+        }
+        
+        private int GetYPos() {
+            int posX = (int)projectile.Bottom.X;
+            int posY = (int)projectile.Bottom.Y;
+
+            if (TerrariaMobaUtils.TileIsSolidOrPlatform(posX / 16, posY / 16)) {
+                while (TerrariaMobaUtils.TileIsSolidOrPlatform(posX / 16, posY / 16)) {
+                    posY -= 1;
+                }
+            }
+            else {
+                while (!TerrariaMobaUtils.TileIsSolidOrPlatform(posX / 16, posY / 16)) {
+                    posY += 1;
+                }
+            }
+            return posY;
+        }
+    }
+}
