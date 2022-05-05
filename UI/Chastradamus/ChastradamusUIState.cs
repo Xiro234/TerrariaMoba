6df5c1111ac3547af99a739483c9a6d8025fec6d
Xiro234 <xiro234@gmail.com>
2022-05-05 10:59:40 -0400

    First work on Chastradamus

---
+++ UI/Chastradamus/ChastradamusUIState.cs
@@ -0,0 +1,15 @@
+﻿using Terraria.UI;
+
+namespace TerrariaMoba.UI.Chastradamus; 
+
+public class ChastradamusUIState: UIState {
+    public static ChastradamusUIState Instance;
+
+    public override void OnInitialize() {
+        Instance = this;
+
+        concoctionWheel = new ConcoctionWheel();
+        Instance.Append(concoctionWheel);
+        concoctionWheel.Hi
+    }
+}
