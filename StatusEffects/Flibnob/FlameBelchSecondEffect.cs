b1021fde164d2b2f6764eb71829d7c2c51fd845c
DreadnovaBlade <worldofmythril@gmail.com>
2023-02-19 10:37:00 +0000

    added self slow whilst casting flame belch + armor shred to melting debuff

--- StatusEffects/Flibnob/FlameBelchSecondEffect.cs
+++ StatusEffects/Flibnob/FlameBelchSecondEffect.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System;
 using System.IO;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using TerrariaMoba.Statistic;
 using TerrariaMoba.StatusEffects.GenericEffects;
+using static TerrariaMoba.Statistic.AttributeType;
 
 namespace TerrariaMoba.StatusEffects.Flibnob; 
 
@@ -12,11 +16,14 @@ public class FlameBelchSecondEffect : DamageOverTime {
     public override string DisplayName { get => "Melting"; } 
     public override Texture2D Icon { get => ModContent.Request<Texture2D>("Textures/Blank").Value; }
 
+    private int armorLoss;
+
     public FlameBelchSecondEffect() { }
-    public FlameBelchSecondEffect(int dmg, int duration, bool canBeCleansed, int applierId) : base(0, 0, dmg, duration, canBeCleansed, applierId) { }
+    public FlameBelchSecondEffect(int armor, int dmg, int duration, bool canBeCleansed, int applierId) : base(0, 0, dmg, duration, canBeCleansed, applierId) {
+        armorLoss = armor;
+    }
 
     public override void WhileActive() {
-        //cuts armor
         var num1 = Dust.NewDust(User.position, User.width, User.height, DustID.Torch, User.velocity.X * 0.2f, User.velocity.Y * 0.2f, 100);
         Dust dust1;
         if (Main.rand.Next(3) < 2) {
@@ -30,13 +37,19 @@ public class FlameBelchSecondEffect : DamageOverTime {
         base.WhileActive();
     }
 
-    public override void SendEffectElements(ModPacket packet) {
+    public override void ConstructFlatAttributes(){
+        FlatAttributes = new Dictionary<AttributeType, Func<float>> {
+                { PHYSICAL_ARMOR, () => armorLoss }
+            };
+    }
 
+    public override void SendEffectElements(ModPacket packet) {
+        packet.Write(armorLoss);
         base.SendEffectElements(packet);
     }
 
     public override void ReceiveEffectElements(BinaryReader reader) {
-
+        armorLoss = reader.ReadInt32();
         base.ReceiveEffectElements(reader);
     }
 }
